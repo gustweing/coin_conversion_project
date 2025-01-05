0d06c5c3ec6c7e62aee3749b85d08ed924741a70
Gustavo Dias <gust.weing@gmail.com>
2025-01-04 23:23:26 -0300

    Commit

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import requests as r
+from datetime import datetime
 
-url = 'https://economia.awesomeapi.com.br/json/daily/USD-BRL/30'
+url = 'https://economia.awesomeapi.com.br/json/daily/USD-BRL/10'
 response = r.get(url)
 
 try:
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        dc_data: dict = {}
 except:
     print('Não foi possível converter')
-
 
