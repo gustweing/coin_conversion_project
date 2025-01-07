e2984766806ddda1ef8307d4b29f8d196509a703
Gustavo Dias <gust.weing@gmail.com>
2025-01-07 09:06:27 -0300

    Alterações código

--- src/main.py
+++ src/main.py
@@ -1,44 +1,67 @@
 import requests as r
 from datetime import datetime
+import time
 
-moedas = [
-'USD-BRL',
-'CAD-BRL',
-'EUR-BRL',
-'GBP-BRL',
-'ARS-BRL',
-'BTC-BRL',
-'LTC-BRL',
-'JPY-BRL',
-'CHF-BRL',
-'AUD-BRL',
-'CNY-BRL',
-'ILS-BRL',
-'ETH-BRL',
-'XRP-BRL'     
-]   
+moeda_mae =['BRL',
+'USD',
+'CAD',
+'EUR',
+'GBP',
+'ARS',
+'BTC',
+'LTC',
+'JPY',
+'CHF',
+'AUD',
+'CNY',
+'ILS',
+'ETH',
+'XRP' ]
+moedas_secundaria = [
+'BRL',
+'USD',
+'CAD',
+'EUR',
+'GBP',
+'ARS',
+'BTC',
+'LTC',
+'JPY',
+'CHF',
+'AUD',
+'CNY',
+'ILS',
+'ETH',
+'XRP'    
+]
 
-datestart = 20240501 
-dateend = 20240505
+j = 0
+num_dias = 2
 filtrados: list = []
-for i in moedas:
-    url = f'https://economia.awesomeapi.com.br/json/daily/{i}/?start_date={datestart}&end_date={dateend}'
-    response = r.get(url)
 
-    if response.status_code == 200:
-            data = response.json()
-    else:
-        print(f'Não foi possível converter devido ao erro {response.status_code}')
-        exit()
+for mae in moeda_mae:
+    for i in moedas_secundaria:
+        url = f'https://economia.awesomeapi.com.br/json/daily/{mae}-{i}/{num_dias}'
+        response = r.get(url)
 
-    for lista in data:
-        filtrados.append({
-            'code': lista.get('code','n/a'), 
-            'codein':lista.get('codein','n/a'), 
-            'max':lista.get('high','n/a'),
-            'min': lista.get('low','n/a'), 
-            'bid': lista.get('bid','n/a'), 
-            'time': lista.get('timestamp','n/a')
-        })
+        try:
+            if response.status_code == 200:
+                data = response.json()
+    
+                for lista in data:
+                    filtrados.append({
+                        'code': lista.get('code',{mae}), 
+                        'codein':lista.get('codein',{i}), 
+                        'max':lista.get('high','n/a'),
+                        'min': lista.get('low','n/a'), 
+                        'bid': lista.get('bid','n/a'), 
+                        'time': lista.get('timestamp','n/a')
+                    })
+        except:
+            print(f'Não foi possível converter devido ao erro {response.status_code}')
+            print(f'Erro ao processar a moeda {i}.')
+        
+print(filtrados)
 
-print(filtrados)    
+
+    
